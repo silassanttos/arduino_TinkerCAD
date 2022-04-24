9c4e30e5d29c38494913470a7bea2f56bfdda6a8
Silas Dos Santos Sousa <69328711+silassanttos@users.noreply.github.com>
2022-04-24 16:14:41 -0300

    Update liga_desliga_led_delay.ino

--- liga_desliga_led_delay.ino
+++ liga_desliga_led_delay.ino
@@ -9,8 +9,8 @@ void setup()
 void loop()
 {
 	digitalWrite(13,HIGH); //O pino 13 recebe uma tensão alta e "LIGA"
-  	delay (3000); //Fica ligado por 3 segundos
+  		delay (3000); //Fica ligado por 3 segundos
   	digitalWrite(13,LOW);// O pino 13 recebe uma tensão baixo e "DESLIGA"
-    delay (1000);//Fica ligado por 1 segundo
+    		delay (1000);//Fica ligado por 1 segundo
 
 }
